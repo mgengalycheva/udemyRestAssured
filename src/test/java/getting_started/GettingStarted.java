1abeabcaeb2f96590561c3be12c25119404a7dde
mgengalycheva <mgengalycheva@gmail.com>
2021-08-31 12:43:25 +0200

    adding tests for getting responce data

--- src/test/java/getting_started/GettingStarted.java
+++ src/test/java/getting_started/GettingStarted.java
@@ -1,5 +1,6 @@
 package getting_started;
 
+import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
@@ -34,10 +35,38 @@ public class GettingStarted {
                 "currencies[0].symbol", equalTo("$"),
                 "languages[0].name", equalTo("English")
             );
+    }
 
+    @Test
+    public void extract_response_data() {
+        Response res = given()
+            .baseUri("https://restcountries.eu/rest/v2").
+        when()
+            .get("/alpha/USA").
+        then()
+            .extract().response();
+        System.out.println(res.asString());
+    }
 
+    @Test
+    public void extract_single_value() {
+        String res = given()
+                .baseUri("https://restcountries.eu/rest/v2").
+                        when()
+                .get("/alpha/USA").
+                        then()
+                .extract().path("capital");
+        System.out.println(res);
+    }
 
-
-
+    @Test
+    public void verify_status_line() {
+        given()
+            .baseUri("https://api.printful.com").
+        when()
+            .get("/variant/1").
+        then()
+            .statusCode(404)
+            .statusLine("HTTP/1.1 404 Not Found");
     }
 }
